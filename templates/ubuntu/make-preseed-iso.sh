2d84119c0680f55b7bc27d7d8a188ea0e8ad475e
Esten Rye <esten@ryezone.net>
2018-07-15 14:10:38 -0500

    Use Ubuntu 16.04. Refactor directory structure.

--- templates/ubuntu/make-preseed-iso.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export MSYS_NO_PATHCONV=1
-SCRIPT_ROOT=$(dirname $0)
-
-
-"$SCRIPT_ROOT/../../bin/cdrtools/win64/mkisofs.exe" -r -R -J -l -o "$SCRIPT_ROOT/preseed.iso" "$SCRIPT_ROOT/http"
